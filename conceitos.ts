22ced409513164c199d6231b329d996219c8fa18
Letícia Andrade <leticiaae@hotmail.com.br>
2023-12-22 19:55:01 -0300

    Add files via upload

---
+++ conceitos.ts
@@ -0,0 +1,15 @@
+// TypeScript é um superset para a linguagem JavaScript
+
+// TypeScript é transpilado para JavaScript por meio de um compilador oficial, ou seja, não executamos em TypeScript
+// instalação do compilaor - npm install typescript --save-dev
+
+// adiciona funções ao JavaScript, como a declaração de tipos de variável (tipagem estática)
+// let nome : string = "Letícia";
+// let idade : number = 21;
+// let desablitado : boolean = false; 
+
+// frameworks e libs que funcionam no Javascript também irá servir para TypeScript 
+
+// Por que trabalhar em typescript se vou executar em javascript?
+
+// programar em typescript facilita a detecção de bugs
