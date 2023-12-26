3209148e559e45eff3569032773a4ac1e26de82b
leticia <leticiaae@hotmail.com.br>
2023-12-26 16:37:59 -0300

    primitives types:: boolean

---
+++ primitive_types/boolean.ts
@@ -0,0 +1,9 @@
+/* boolean um tipo de dados primitivo no TypeScript que representa um valor booleano, 
+ou seja, verdadeiro ou falso.  */
+
+let desabilitado: boolean = true; // declaração que pode ser TRUE ou FALSE
+desabilitado = false;
+
+let estaDoente: boolean = false;
+estaDoente = true;
+
