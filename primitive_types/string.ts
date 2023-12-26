48762adab58ed92546592a4ae4ae711379636bb1
leticia <leticiaae@hotmail.com.br>
2023-12-26 16:41:24 -0300

    primitive types:: string

---
+++ primitive_types/string.ts
@@ -0,0 +1,6 @@
+/* É um tipo de dados primitivo no TypeScript que representa dados textuais. 
+É um conjunto de elementos do conjunto de caracteres Unicode de 16 bits. */
+
+let texto: string = "solta o som dj!"; 
+
+let texto2: string = "posso adicionar o que for né! Em 2023 ainda"
