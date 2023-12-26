bae8882519623b66622571e1a33cd18d0871ee88
leticia <leticiaae@hotmail.com.br>
2023-12-26 16:34:35 -0300

    primitive types:: number

---
+++ primitive_types/number.ts
@@ -0,0 +1,5 @@
+/* É um tipo de dados primitivo no TypeScript que representa valores numéricos. 
+Inclui valores inteiros e de ponto flutuante. */
+
+let numero1: number = 3; // numero inteiro
+let numero2: number = 3.2; // ponto flutuante recebe o mesmo tipo NUMBER SE LIGA! 
