dcebdb079041d425d4e3354eed322889334a0319
leticia <leticiaae@hotmail.com.br>
2023-12-27 18:06:19 -0300

    object types:: tuples

---
+++ object_types/tuples.ts
@@ -0,0 +1,16 @@
+/* Um tipo de tupla é outro tipo de Array que sabe exatamente quantos elementos ele contém 
+e exatamente quais tipos ele contém em posições específicas. */
+
+let tupla: [string, number] = ["oi", 19]
+
+console.log(tupla) // saída ["oi", 19]
+
+function testeTupla( tupla: [number, string, boolean]) {
+    const a = tupla[0]
+    const b = tupla[1]
+    const c = tupla[2]
+    console.log(a, b, c)
+
+}
+
+testeTupla([21, "oiaaa", true]) // saída [21, "oiaaa", true]
