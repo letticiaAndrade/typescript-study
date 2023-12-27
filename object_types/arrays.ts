43dec6d052b753fc19e65648858aebbd1aefc984
leticia <leticiaae@hotmail.com.br>
2023-12-27 17:08:55 -0300

    object types:: arrays

---
+++ object_types/arrays.ts
@@ -0,0 +1,11 @@
+/* Para declarar um array em typescript, ele funciona com todos os tipos */
+
+// numeros
+let resultados: number[]; //
+let valores: number[] = [1,2,3,3]; // preenchido
+let idades: Array<number>;
+
+// strings
+let series: string[];
+let nomesFemininos: string[] = ["Letícia", "Lele", "Le"];
+let apelidos: Array<string>; // essa declaração é a mesma que a de cima
