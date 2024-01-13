111fcef844fd76bbac399d818981aca38221182b
leticia <leticiaae@hotmail.com.br>
2024-01-13 15:02:41 -0300

    feat:other types:: difference ANY X UNKNOWN

--- other_types/unknown.ts
+++ other_types/unknown.ts
@@ -2,7 +2,6 @@
 uma atribuição de tipo antes de utilizá-la. Isso incentiva a escrita de código mais seguro e explícito. */
 
 // Ao contrário do tipo “any”, o “unknown” exige uma maior garantia de segurança no uso de valores.
-
 function verificacaoTipo(value: unknown) {
     if (typeof value === 'string') {
           console.log('O valor é uma string:', value.toUpperCase());
@@ -19,3 +18,9 @@ verificacaoTipo(data); // saída: O valor é uma string: ESTOU ESTUDANDO TYPESCR
 /* O “unknown” promove a segurança de tipo, exigindo verificações e atribuições explícitas antes do 
 uso de valores desconhecidos, enquanto o “any” oferece maior flexibilidade, mas com menos
  garantias de tipo. */
+
+/* DIFERENÇAS ANY X UNKNOWN
+1.Verificações de tipo: O “unknown” requer verificações de tipo ou atribuições explícitas antes de utilizar seus valores, enquanto o “any” não exige essas verificações.
+2.Segurança de tipo: O “unknown” oferece maior segurança de tipo, pois incentiva a escrita de código mais seguro e explícito, enquanto o “any” não impõe restrições e pode resultar em perda de tipo.
+3.Inferência de tipo: O “unknown” não permite inferência automática de tipo, exigindo que o desenvolvedor forneça informações de tipo explicitamente, ao contrário do “any”, que permite a inferência automática de tipo.
+4.Compatibilidade de tipo: O “unknown” é menos compatível com outros tipos do que o “any”, pois requer verificação de tipo explícita para a maioria das operações, enquanto o “any” é amplamente compatível com qualquer tipo. */
