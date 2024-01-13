dd03035eadb8631b947cf4e972a9899563b6d328
leticia <leticiaae@hotmail.com.br>
2024-01-13 20:24:09 -0300

    feat:combination types::  union

---
+++ combination_types/union.ts
@@ -0,0 +1,22 @@
+/* Tipos de União no TypeScript permitem que você especifique vários tipos possíveis para uma única 
+variável ou parâmetro. Um tipo de união é escrito como uma barra vertical | lista separada de tipos. */
+
+function testeUniao(x:string | number) {
+    console.log(x)
+}
+
+testeUniao(1) // saída: 
+testeUniao("deu bom!") // saída:
+testeUniao(true) // ERRO, a função só aceita string e number como parâmetros 
+
+// ERRADO
+function senha(x: string | number) {
+    console.log(x.toUpperCase()) // precisa ser uma função que exista para todos os tipos, nesse caso string e number 
+}
+
+// CORRETO
+function senha2(x: string | number) {
+    console.log(x) 
+}
+
+senha2("minhaSenha") // saída: "minhaSenha"
